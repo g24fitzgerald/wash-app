84fb7d2ef3e3b2db7170782aba637fda83235283
Vinh Luu <vluu87@gmail.com>
2017-01-14 17:44:36 -0600

    styling login page

--- washApp/components/Login.js
+++ washApp/components/Login.js
@@ -19,10 +19,11 @@ export default class Login extends Component {
         <View style={styles.loginIcons}><Text>Icons</Text></View>
         <View style={styles.loginHero}><Text>Hero</Text>
           <TouchableHighlight style={styles.button}>
-            <Text style={styles.buttonText}>Reset Values to 2</Text>
+            <Text style={styles.buttonText}>Login</Text>
           </TouchableHighlight>
         </View>
-        <View style={styles.loginFooter}><Text>Footer</Text></View>
+        <View style={styles.loginFooter}>
+          <Text style={styles.footerText}>How It Works</Text></View>
       </View>
     );
   }
@@ -31,35 +32,69 @@ export default class Login extends Component {
 const styles = StyleSheet.create({
   containerLogin: {
     flex: 6,
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'pink',
+    backgroundColor: 'white',
+    flexDirection: 'column',
   },
   loginTitle: {
-    backgroundColor: 'yellow',
-    flex: 1
+    backgroundColor: '#1AAEED',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+    width: 375,
+    fontSize: 50,
+    fontWeight: 'bold',
+    marginBottom: 10,
+
   },
   loginIcons: {
-    backgroundColor: 'blue',
-    flex: 1
+    backgroundColor: '#37D4D4',
+    flex: 1,
+    width: 375,
+    alignItems: 'center',
+    marginBottom: 10,
+
   },
   loginHero: {
     backgroundColor: 'yellow',
-    flex: 3
+    flex: 3,
+    width: 375,
+    alignItems: 'center',
+    marginBottom: 10,
+
   },
   loginFooter: {
-    backgroundColor: 'blue',
-    flex: 1
+    backgroundColor: '#1AAEED',
+    flex: 1,
+    width: 375,
+    alignItems: 'center',
+
+
   },
   button: {
-  backgroundColor: '#1452E3',
+  backgroundColor: 'white',
   flex: 1,
   justifyContent: 'center',
-  alignItems: 'center'
+  alignItems: 'center',
+  maxHeight: 10,
+  
+
   },
+
   buttonText: {
-    fontSize: 20
+  fontSize: 20,
+  color: 'black'
+  },
+
+  footerText: {
+    fontSize: 25,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-end'
+
   }
+
 });
 
 AppRegistry.registerComponent('washApp', () => Login);
