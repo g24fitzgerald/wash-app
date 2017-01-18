34734c11665811906c31bbbeacd528528460fe78
Vinh Luu <vluu87@gmail.com>
2017-01-18 10:52:14 -0600

    working on process page details

--- washApp/pages/Process.js
+++ washApp/pages/Process.js
@@ -18,13 +18,13 @@ export default class washApp extends Component {
           source={require('../images/process.png')}
           style={styles.backgroundLogo} />
         <View style={styles.container}>
+          <Text style={styles.title}>AUSTIN'S PREMIER WASH AND FOLD SERVICE {"\n"}</Text>
           <Text style={styles.header}>OUR PROCESS</Text>
-
-          <Text style={styles.text}>1. Create an account and schedule a pick-up time any day of the week from 8am to 8pm. {"\n"}{"\n"}
+          <Text style={styles.text}>1. Create an account and schedule a pick-up time for any day of the week between 8am to 8pm. {"\n"}{"\n"}
 2. Schedule a drop-off time for your freshly washed and folded laundry. {"\n"}{"\n"}
-3. Our valet will collect your goods at the scheduled time and transport to our CT Laundry facility. {"\n"}{"\n"}
-4. The cleaning attendants will carefully service your goods under your specific instructions and preferences. {"\n"}{"\n"}
-5. Once your order has finished, it will be sent out for delivery at the chosen time. </Text>
+3. Our valet will collect your goods at the requested time and transport to our locally operated facilities. {"\n"}{"\n"}
+4. The cleaning attendants will carefully service your clothes and linen under your specific instructions and preferences. {"\n"}{"\n"}
+5. Once your order has finished, you will receive a notification, and it will be sent out for delivery at the chosen time. </Text>
         </View>
       </View>
     );
@@ -49,20 +49,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 3,
   },
-  header: {
-    color: '#1AAEED',
+  title: {
+    color: 'black',
     fontSize: 16,
     width: 370,
     textAlign: 'center',
     fontWeight: 'bold',
-    marginTop: 20,
+    marginTop: 10,
+  },
+  header: {
+    color: '#1AAEED',
+    fontSize: 14,
+    width: 370,
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
   text: {
     color: '#1AAEED',
     fontSize: 14,
     width: 350,
     textAlign: 'left',
-    fontWeight: 'bold',
     marginTop: 10,
     marginRight: 15,
     marginLeft: 15,
