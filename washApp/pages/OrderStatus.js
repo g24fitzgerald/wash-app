f97c4c1c98486e6e849b0882515d405c6d1875ec
Gina Fitzgerald <g24fitzgerald@gmail.com>
2017-01-18 16:45:50 -0600

    orderStatus functionality

--- washApp/pages/OrderStatus.js
+++ washApp/pages/OrderStatus.js
@@ -7,10 +7,11 @@ import {
   Text,
   View
 } from 'react-native';
-
+import { firebase } from '../utils/firebase';
 export default class OrderStatus extends Component {
   constructor(props){
     super(props)
+    this.firebaseRef = firebase.database().ref('users/').child('users')
     this.state = {
       orders: []
     }
@@ -26,6 +27,11 @@ export default class OrderStatus extends Component {
       // dropoffDate: '',
       // dropoffTime: '',
     })
+    let user= firebase.auth().currentUser;
+    console.log('this.firebaseRef', this.firebaseRef);
+    console.log("firebase call" + user);
+    console.log('this.props.firebase', this.props.firebase);
+    console.log( 'this.props.firebase.auth().currentUser',this.props.firebase.auth().currentUser);
     console.log('heres this stuff ', this.props.firebase.database().ref('/users/'+this.state.uid+'/orders/')  );
     console.log('heres the second thing', this.props.firebase.database().ref('/users/'+this.state.uid+'/orders/-KamZUkIHm1ucbHZdFPS'));
 
