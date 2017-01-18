3feb680cc2d5751c68f61b09166a15249bd45fba
Gina Fitzgerald <g24fitzgerald@gmail.com>
2017-01-18 15:40:08 -0600

    broken order status

--- washApp/pages/OrderStatus.js
+++ washApp/pages/OrderStatus.js
@@ -9,18 +9,42 @@ import {
 } from 'react-native';
 
 export default class OrderStatus extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      orders: []
+    }
+  }
+  componentWillMount(){ //set up new component when page is going to load with the following properties set.
+    const userData = this.props.firebase.auth().currentUser;
+    this.setState({
+      loading: false,
+      uid: userData.uid,
+      // pickupDate: userData.uid,
+      // pickupTime: userData.uid,
+      // dropoff: '',
+      // dropoffDate: '',
+      // dropoffTime: '',
+    })
+    console.log('heres this stuff ', this.props.firebase.database().ref('/users/'+this.state.uid+'/orders/')  );
+    console.log('heres the second thing', this.props.firebase.database().ref('/users/'+this.state.uid+'/orders/-KamZUkIHm1ucbHZdFPS'));
+
+  }
+  // const renderOrders = this.state.orders.map(todo => {
+  //     return(
+  //       <li key={todo.key} className="todo-item">
+  //         { todo.val }
+  //       </li>
+  //     )
+  //   })
   render() {
     return (
-      <View style={styles.container}>
-        <Text>
-        Order Status
-        </Text>
-      </View>
+      <Text>Order status</Text>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const stylesOrder = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
