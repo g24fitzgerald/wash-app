259f67282bf7e46fb9ad504968e3ea13be74c09b
Daniel Nguyen <dtn601@gmail.com>
2017-01-19 12:29:31 -0600

    set up profile edit functionality

--- washApp/pages/Profile.js
+++ washApp/pages/Profile.js
@@ -15,6 +15,7 @@ import {
 } from 'react-native';
 
 import styles from '../styles/common-styles';
+import Account from './Account'
 
 export default class Profile extends Component {
     constructor(props) {
@@ -31,14 +32,6 @@ export default class Profile extends Component {
         city: '',
         state: '',
         zip: '',
-        style: {
-                height: 30,
-                width: 150,
-                borderColor: '#1AAEED',
-                borderWidth: 1,
-                margin: 2,
-                fontSize: 15
-          }
        }
     }
 
@@ -67,11 +60,34 @@ export default class Profile extends Component {
     }
 
   handleSubmit(){
+    const userData = this.props.firebase.auth().currentUser;
+    this.props.firebase.database()
+    .ref('/users/'+userData.uid)
+    .update({
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            phoneNumber: this.state.phoneNumber
+          });
+    this.props.firebase.database()
+    .ref('/users/'+userData.uid+'/location/')
+    .update({
+            address1: this.state.address1,
+            address2: this.state.address2,
+            city: this.state.city,
+            state: this.state.state,
+            zip: this.state.zip
+              });
 
+  alert('Profile Updated!')
+    this.props.navigator.push({ //dictates which page the navigator will display next
+      component: Profile
+    })
   }
 
   handleBack(){
-    this.props.navigator.pop()
+    this.props.navigator.push({
+      component: Account
+    })
   }
   render() {
           return (
@@ -89,6 +105,7 @@ export default class Profile extends Component {
                   <Text style={pageStyles.fontSM}> Last Name</Text>
                   <TextInput
                     style={pageStyles.textInput}
+                    onChangeText={(text) => this.setState({lastName: text})}
                     value={ this.state.lastName }
                     placeholder={ this.state.lastName } />
 
@@ -101,36 +118,42 @@ export default class Profile extends Component {
                   <Text style={pageStyles.fontSM}> Phone Number </Text>
                   <TextInput
                     style={pageStyles.textInput}
+                    onChangeText={(text) => this.setState({phoneNumber: text})}
                     value={ this.state.phoneNumber }
                     placeholder={ this.state.phoneNumber } />
 
                   <Text style={pageStyles.fontSM}> Address 1 </Text>
                   <TextInput
                     style={pageStyles.textInput}
+                    onChangeText={(text) => this.setState({address1: text})}
                     value={ this.state.address1 }
                     placeholder={ this.state.address1 } />
 
                   <Text style={pageStyles.fontSM}> Address 2 </Text>
                   <TextInput
                     style={pageStyles.textInput}
+                    onChangeText={(text) => this.setState({address2: text})}
                     value={ this.state.address2 }
                     placeholder={ this.state.address2 } />
 
                   <Text style={pageStyles.fontSM}> City </Text>
                   <TextInput
                     style={pageStyles.textInput}
+                    onChangeText={(text) => this.setState({city: text})}
                     value={ this.state.city }
                     placeholder={ this.state.city } />
 
                   <Text style={pageStyles.fontSM}> State </Text>
                   <TextInput
                     style={pageStyles.textInput}
+                    onChangeText={(text) => this.setState({state: text})}
                     value={ this.state.state }
                     placeholder={ this.state.state } />
 
                   <Text style={pageStyles.fontSM}> Zip </Text>
                   <TextInput
                     style={pageStyles.textInput}
+                    onChangeText={(text) => this.setState({zip: text})}
                     value={ this.state.zip }
                     placeholder={ this.state.zip } />
 
